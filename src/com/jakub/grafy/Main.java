69b9ffec17971ff8cc7a1c976f39efb40d6e29d2
Jakub Karolczak <hobkinss@gmail.com>
2015-10-29 19:08:30 +0100

    Creating temp copy of matrix for safety

--- src/com/jakub/grafy/Main.java
+++ src/com/jakub/grafy/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	static ArrayList<ArrayList<?>> matrix = new ArrayList<ArrayList<?>>();
+	static ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
 	static ArrayList<Integer> center = new ArrayList<>();
 	static ArrayList<Integer> allVert = new ArrayList<>();
 	static int radius;
@@ -29,12 +29,22 @@ public class Main {
 			case(2):
 				center.clear();
 				allVert.clear();
+				
+				//Tworzê tymczasow¹ kopiê macierzy, ¿eby unikn¹æ nadpisañ
+				ArrayList<ArrayList<Integer>> tempTree = new ArrayList<ArrayList<Integer>>();
+				for(int i = 0; i < matrix.size(); i++){
+					tempTree.add(new ArrayList<Integer>());
+					for(int j = 0; j < (matrix.get(i)).size(); j++){
+						tempTree.get(i).add( matrix.get(i).get(j) );
+					}
+				}
+				
 				//Spisujê wszystkie wierzcho³ki na potrzeby listy pomocniczej
 				for(int i = 0; i < matrix.size(); i++){
 					allVert.add(i);
 				}
 				
-				radius = findCenterV1(matrix, new ArrayList<Integer>(), allVert);
+				radius = findCenterV1(tempTree, new ArrayList<Integer>(), allVert);
 				System.out.print("Centrum drzewa: ");
 				
 				for(int i = 0; i < center.size(); i++){
@@ -43,6 +53,7 @@ public class Main {
 				
 				System.out.println("");
 				System.out.println("Œrednica: " + radius);
+				tempTree.clear();
 				break;
 			}
 		}
@@ -50,7 +61,7 @@ public class Main {
 	}
 
 	// Wczytywanie macierzy z pliku
-	@SuppressWarnings({ "unchecked" })
+	@SuppressWarnings({ })
 	public static void loadMatrix() throws Exception {
 		File file = new File("Macierz.txt");
 		Scanner sc = new Scanner(file);
@@ -60,9 +71,9 @@ public class Main {
 		int lineCount = 0;
 		while (sc.hasNextLine()) {
 			String[] currentLine = sc.nextLine().trim().split(" ");
-			matrix.add(new ArrayList<Object>());
+			matrix.add(new ArrayList<Integer>());
 			for (int i = 0; i < currentLine.length; i++) {
-				((ArrayList<Integer>) matrix.get(lineCount)).add(Integer.parseInt(currentLine[i]));
+				(matrix.get(lineCount)).add(Integer.parseInt(currentLine[i]));
 			}
 			lineCount++;
 		}
@@ -70,7 +81,6 @@ public class Main {
 	}
 
 	// Stopieñ wierzcho³ka
-	@SuppressWarnings("unchecked")
 	public static int vertDeg(int vert) {
 		int deg;
 		int loops = 0;
@@ -78,9 +88,9 @@ public class Main {
 
 		for (int i = 0; i < matrix.size(); i++) {
 			if (i == vert)
-				loops = ((ArrayList<Integer>) matrix.get(vert)).get(i);
+				loops = (matrix.get(vert)).get(i);
 			else
-				edges += ((ArrayList<Integer>) matrix.get(vert)).get(i);
+				edges += (matrix.get(vert)).get(i);
 		}
 
 		deg = 2 * loops + edges;
@@ -88,8 +98,7 @@ public class Main {
 	}
 	
 	//Szukanie centrum
-	@SuppressWarnings("unchecked")
-	public static int findCenterV1(ArrayList<ArrayList<?>> tree, ArrayList<Integer> leaves, ArrayList<Integer> vert){
+	public static int findCenterV1(ArrayList<ArrayList<Integer>> tree, ArrayList<Integer> leaves, ArrayList<Integer> vert){
 		int edges;
 		
 		//Znaleziono centrum drzewa
@@ -102,20 +111,20 @@ public class Main {
 		if(leaves.size() > 0){
 			for(int i = 0; i < leaves.size(); i++){
 				for(int j = 0; j < tree.size(); j++){
-					((ArrayList<Integer>) tree.get(leaves.get(i))).set(j, 0);
-					((ArrayList<Integer>) tree.get(j)).set(leaves.get(i), 0);
+					(tree.get(leaves.get(i))).set(j, 0);
+					(tree.get(j)).set(leaves.get(i), 0);
 				}
 				//Usuwam wierzcho³ek z listy wszystkich wierzcho³ków
 				vert.remove(Integer.valueOf(leaves.get(i)));
 			}
 		}
-		leaves.clear();
+		leaves = new ArrayList<Integer>();
 		
 		//Wyszukujê wszystkich liœci
 		for(int i = 0; i < tree.size(); i++){
 			edges = 0;
 			for(int j = 0; j < tree.size(); j++){
-				if(((ArrayList<Integer>)tree.get(i)).get(j) == 1){
+				if((tree.get(i)).get(j) == 1){
 					edges++;
 				}
 			}
